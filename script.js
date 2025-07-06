4c406bf70bcf59dfd3b6674de9b56eee87782c6f
Ian Omondi <ianomondi9011@gmail.com>
2025-07-06 20:34:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Theme toggle
+themeToggle.addEventListener('change', () => {
+  if (themeToggle.checked) {
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+  }
+});
